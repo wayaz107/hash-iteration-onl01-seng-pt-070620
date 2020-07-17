432de2040ef750727725ff5fa4fb5fce34a38b41
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 00:16:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
@@ -9,7 +9,4 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name,age|
     puts "Happy Birthday #{kids_name}!You are now #{age} years old!"
 end
-end 
-
-
-
+end
