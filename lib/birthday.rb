cce62012e415472944188ed57cbb0ad4c666ab7a
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 00:27:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,10 +13,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name,age|
-         if age <=12 
-            "Happy Birthday #{kids_name}! You are now #{age} years old!" 
+         if age <=12
+            "Happy Birthday #{kids_name}! You are now #{age} years old!"
        else
          "You are too old for this."
-       end 
+       end
 end
 end
